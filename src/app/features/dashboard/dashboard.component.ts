e9436ff941463e30737a01c3755d226719f3d0be
Manas Das <dasmanas2308@gmail.com>
2025-05-16 18:59:09 +0530

    feat: add new SVG icons for network and play, update dashboard layout with search functionality and category filtering

--- src/app/features/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/dashboard.component.ts
@@ -5,37 +5,49 @@ import { GameServiceService } from '../../core/services/game-service.service';
 import { GameCardComponent } from "./game-card/game-card.component";
 import { CommonModule } from '@angular/common';
 import { SidebarComponent } from "../../layout/sidebar/sidebar.component";
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [FlashScreenComponent, GameCardComponent, CommonModule, SidebarComponent],
+  imports: [FlashScreenComponent, GameCardComponent, CommonModule, SidebarComponent,FormsModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
-  //  games = Array(10).fill({});
-
-
-  constructor(private gameService: GameServiceService) { }
-
   public showFlash = true;
   public games: Game[] = [];
+  public allGames: Game[] = [];
   public categories: string[] = [];
   public selectedCategory = 'all';
+  public searchTerm = '';
+
+  constructor(private gameService: GameServiceService) {}
 
   ngOnInit() {
-    setTimeout(() => {
-      this.showFlash = false;
-    }, 5000);
-
+    setTimeout(() => (this.showFlash = false), 5000);
     this.categories = this.gameService.getCategories();
-    this.games = this.gameService.getGames();
+    this.allGames = this.gameService.getGames();
+    this.filterGames();
   }
-
 
   filterByCategory(category: string): void {
     this.selectedCategory = category;
-    this.games = this.gameService.getGamesByCategory(category);
+    this.filterGames();
+  }
+
+  onSearchChange(): void {
+    this.filterGames();
+  }
+
+  private filterGames(): void {
+    const filteredByCategory =
+      this.selectedCategory === 'all'
+        ? this.allGames
+        : this.gameService.getGamesByCategory(this.selectedCategory);
+
+    this.games = filteredByCategory.filter(game =>
+      game.title.toLowerCase().includes(this.searchTerm.toLowerCase())
+    );
   }
 
 
