c94dc0c94c5be826808809f861b97b78d07c59f8
Manas Das <dasmanas2308@gmail.com>
2025-05-19 12:04:09 +0530

    feat: implement GamePlayer component with game viewer and chat room functionality

--- src/app/features/dashboard/game-player/game-player.component.ts
+++ src/app/features/dashboard/game-player/game-player.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  Component,
+  ElementRef,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'app-game-player',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './game-player.component.html',
   styleUrl: './game-player.component.scss'
 })
 export class GamePlayerComponent {
 
+
+    @ViewChild('iframeRef') iframeRef!: ElementRef<HTMLIFrameElement>;
+
+  handleFullscreen(): void {
+    const iframe = this.iframeRef.nativeElement;
+    if (iframe.requestFullscreen) {
+      iframe.requestFullscreen();
+    } else if ((iframe as any).webkitRequestFullscreen) {
+      (iframe as any).webkitRequestFullscreen(); // Safari
+    } else if ((iframe as any).msRequestFullscreen) {
+      (iframe as any).msRequestFullscreen(); // IE
+    }
+  }
 }
