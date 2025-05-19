fecf3ba8771348b22452db40bdf221aff408bacf
Manas Das <dasmanas2308@gmail.com>
2025-05-19 12:02:29 +0530

    feat: add GamePlayer component with template, styles, and unit tests

---
+++ src/app/features/dashboard/game-player/game-player.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-game-player',
+  imports: [],
+  templateUrl: './game-player.component.html',
+  styleUrl: './game-player.component.scss'
+})
+export class GamePlayerComponent {
+
+}
